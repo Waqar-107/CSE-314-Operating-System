2ecdd6d0c9367065f4194db997eb95cf4ffb6aa8
Waqar-107 <waqar.hassan866@gmail.com>
2018-11-09 18:13:38 +0600

    upd

--- Shell-Practice/problem3.sh
+++ Shell-Practice/problem3.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 #from dust i have come dust i will be
 
+ls -l
+
+#newline
+echo 
+
+for file in $*
+do
+    #check if the file is regular
+    if [ -f file ]; then
+        chmod a-x file
+    fi
+done
+
+ls -l
+
---
+++ Shell-Practice/problem4.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#from dust i have come dust i will be
+
