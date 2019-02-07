00d9e4d18e1ea4572478ed200b92b44c87555eb1
Waqar-107 <waqar.hassan866@gmail.com>
2019-02-07 13:24:04 +0600

    ready to read

--- xv6_offline_5/vm.c
+++ xv6_offline_5/vm.c
@@ -333,7 +333,12 @@ copyuvm(pde_t *pgdir, uint sz)
       goto bad;
     memmove(mem, (char*)P2V(pa), PGSIZE);
     if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0) {
+
+      //========================================================================
+      // added by rifat sir	
       kfree(mem);
+      //========================================================================
+      
       goto bad;
     }
   }
